878992c1c322b74f78f814fbfee2ebc62bafddbb
Yogesh-24 <84395954+Yogesh-24@users.noreply.github.com>
2026-08-22 17:09:49 +0530

    Update generate-page-metadata.ts

--- src/utils/seo/generate-page-metadata.ts
+++ src/utils/seo/generate-page-metadata.ts
@@ -66,9 +66,10 @@ export function generateMetadata({
     },
     icons: {
       icon: [
-        { url: "/favicon.ico" },
-        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        { url: "/icon.svg", type: "image/svg+xml" },
+        // { url: "/favicon.ico" },
+        // { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+        // { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       ],
       apple: [
         { url: "/apple-icon-180x180.png", sizes: "180x180", type: "image/png" },
